e0adf937e83f76dd19dc1f3ae7deeae2e762a52c
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2022-12-05 10:39:51 +0100

    :sparkles: Advanced Techniques

---
+++ advanced techniques/threejs_journey-code_structuring_complex_projects/readme.md
@@ -0,0 +1,14 @@
+# Three Js Structure for complex projects
+
+## :gear: Setup
+
+```bash
+# Install dependencies (only the first time)
+npm install
+
+# Run the local server at localhost:8080
+npm run dev
+
+# Build for production in the dist/ directory
+npm run build
+```
---
+++ advanced techniques/threejs_journey-realistic_render/src/script.js
@@ -0,0 +1,212 @@
+import './style.css';
+import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'lil-gui';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+
+/**
+ * Loaders
+ */
+const gltfLoader = new GLTFLoader();
+const cubeTextureLoader = new THREE.CubeTextureLoader();
+
+/**
+ * Base
+ */
+// Debug
+const gui = new dat.GUI();
+const debugObject = {};
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl');
+
+// Scene
+const scene = new THREE.Scene();
+
+/**
+ * Sizes
+ */
+const sizes = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+window.addEventListener('resize', () => {
+  // Update sizes
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.set(4, 1, -4);
+scene.add(camera);
+
+/**
+ * Update all Materials
+ */
+const updateAllMaterial = () => {
+  scene.traverse((child) => {
+    if (
+      child instanceof THREE.Mesh &&
+      child.material instanceof THREE.MeshStandardMaterial
+    ) {
+      // child.material.envMap = environmentMap;
+      child.material.envMapIntensity = debugObject.envMapIntensity;
+      child.material.needsUpdate = true;
+      child.castShadow = true;
+      child.receiveShadow = true;
+    }
+  });
+};
+
+/**
+ * Environment Map
+ */
+const environmentMap = cubeTextureLoader.load([
+  './textures/environmentMaps/0/px.jpg',
+  './textures/environmentMaps/0/nx.jpg',
+  './textures/environmentMaps/0/py.jpg',
+  './textures/environmentMaps/0/ny.jpg',
+  './textures/environmentMaps/0/pz.jpg',
+  './textures/environmentMaps/0/nz.jpg',
+]);
+environmentMap.encoding = THREE.sRGBEncoding;
+scene.background = environmentMap;
+scene.environment = environmentMap;
+
+debugObject.envMapIntensity = 1;
+gui
+  .add(debugObject, 'envMapIntensity')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .onChange(updateAllMaterial);
+
+/**
+ * Models
+ */
+gltfLoader.load('/models/hamburger.glb', (gltf) => {
+  console.log('success');
+  gltf.scene.scale.set(0.3, 0.3, 0.3);
+  gltf.scene.position.set(0, -1, 0);
+  scene.add(gltf.scene);
+
+  gui
+    .add(gltf.scene.rotation, 'y')
+    .min(-Math.PI)
+    .max(Math.PI)
+    .step(0.001)
+    .name('Rotation');
+  //------------------
+
+  updateAllMaterial();
+});
+
+/**
+ * Lights
+ */
+// Directional Light
+const directionalLight = new THREE.DirectionalLight('#ffffff', 3);
+directionalLight.position.set(0.25, 3, -2.25);
+directionalLight.castShadow = true;
+directionalLight.shadow.camera.far = 15;
+directionalLight.shadow.mapSize.set(1024, 1024);
+directionalLight.shadow.normalBias = 0.05;
+scene.add(directionalLight);
+
+// Helper
+// const directionalLightCameraHelper = new THREE.CameraHelper(
+//   directionalLight.shadow.camera
+// );
+// scene.add(directionalLightCameraHelper);
+
+gui
+  .add(directionalLight, 'intensity')
+  .min(0)
+  .max(10)
+  .step(0.001)
+  .name('Light Intensity');
+
+gui
+  .add(directionalLight.position, 'x')
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .name('lightX');
+gui
+  .add(directionalLight.position, 'y')
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .name('lightY');
+gui
+  .add(directionalLight.position, 'z')
+  .min(-5)
+  .max(5)
+  .step(0.001)
+  .name('lightZ');
+
+// Controls
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+// To have the same light in blender
+renderer.physicallyCorrectLights = true;
+renderer.outputEncoding = THREE.sRGBEncoding;
+renderer.toneMapping = THREE.ReinhardToneMapping;
+renderer.toneMappingExposure = 1;
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
+
+gui.add(renderer, 'toneMapping', {
+  No: THREE.NoToneMapping,
+  Linear: THREE.LinearToneMapping,
+  Reinhard: THREE.ReinhardToneMapping,
+  Cineon: THREE.CineonToneMapping,
+  ACESFilmic: THREE.ACESFilmicToneMapping,
+});
+
+gui.add(renderer, 'toneMappingExposure').min(0).max(10).step(0.001);
+
+/**
+ * Animate
+ */
+const tick = () => {
+  // Update controls
+  controls.update();
+
+  // Render
+  renderer.render(scene, camera);
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick);
+};
+
+tick();
