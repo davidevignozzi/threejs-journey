53551e0c7c8c3bfef5cf600554fe92c665354bc3
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-19 16:59:35 +0100

    :sparkles: Portal scene fireflies setup

--- threejs_journey-portal_scene/src/script.js
+++ threejs_journey-portal_scene/src/script.js
@@ -92,6 +92,37 @@ gltfLoader.load('portal.glb', (gltf) => {
 });
 
 /**
+ * Fireflies
+ */
+const firefliesGeometry = new THREE.BufferGeometry();
+const firefliesCount = 30;
+const positionArray = new Float32Array(firefliesCount * 3);
+
+for (let i = 0; i < firefliesCount; i++) {
+  positionArray[i * 3 + 0] = (Math.random() - 0.5) * 4;
+  positionArray[i * 3 + 1] = Math.random() * 1.5;
+  positionArray[i * 3 + 2] = (Math.random() - 0.5) * 4;
+}
+
+firefliesGeometry.setAttribute(
+  'position',
+  new THREE.BufferAttribute(positionArray, 3)
+);
+
+// Material
+const firefliesMaterial = new THREE.PointsMaterial({
+  size: 0.1,
+  sizeAttenuation: true,
+});
+
+// Points
+const fireflies = new THREE.Points(
+  firefliesGeometry,
+  firefliesMaterial
+);
+scene.add(fireflies);
+
+/**
  * Sizes
  */
 const sizes = {
