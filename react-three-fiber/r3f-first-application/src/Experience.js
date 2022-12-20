2b422f30e5e17a219798e9adf11156711378db03
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-20 16:55:24 +0100

    :green_heart: Practise

--- react-three-fiber/r3f-first-application/src/Experience.js
+++ react-three-fiber/r3f-first-application/src/Experience.js
@@ -9,7 +9,27 @@ import React from 'react';
 // Don't have to provide mesh
 
 const Experience = () => {
-    return <></>;
+    return (
+        <>
+            {/* Sphere */}
+            <mesh position-x={-2}>
+                <sphereGeometry />
+                <meshBasicMaterial color="orange"></meshBasicMaterial>
+            </mesh>
+
+            {/* Cube */}
+            <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+                <boxGeometry scale={1.5} />
+                <meshBasicMaterial color="mediumpurple"></meshBasicMaterial>
+            </mesh>
+
+            {/* Plane / Floor */}
+            <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+                <planeGeometry />
+                <meshBasicMaterial color="greenyellow"></meshBasicMaterial>
+            </mesh>
+        </>
+    );
 };
 
 const oldStff = () => {
