e464a3622c0421e9165d4dfc078c86558784b770
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-21 11:16:52 +0100

    :sparkles: Gruop handling

--- react-three-fiber/r3f-first-application/src/Experience.js
+++ react-three-fiber/r3f-first-application/src/Experience.js
@@ -12,27 +12,36 @@ import { useFrame } from '@react-three/fiber';
 const Experience = () => {
     // Refs
     const cubeRef = useRef();
+    const groupRef = useRef();
 
     // On each frame
     useFrame((state, delta) => {
-        // Animate cube
+        /**
+         * Animate
+         */
+        // Update cube;
         // cubeRef.current.rotation.y -= 0.01; => Different Fps? problem
         cubeRef.current.rotation.y -= delta;
+
+        // Update group
+        groupRef.current.rotation.y += delta;
     });
 
     return (
         <>
-            {/* Sphere */}
-            <mesh position-x={-2}>
-                <sphereGeometry />
-                <meshBasicMaterial color="orange"></meshBasicMaterial>
-            </mesh>
+            <group ref={groupRef}>
+                {/* Sphere */}
+                <mesh position-x={-2}>
+                    <sphereGeometry />
+                    <meshBasicMaterial color="orange"></meshBasicMaterial>
+                </mesh>
 
-            {/* Cube */}
-            <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
-                <boxGeometry scale={1.5} />
-                <meshBasicMaterial color="mediumpurple"></meshBasicMaterial>
-            </mesh>
+                {/* Cube */}
+                <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+                    <boxGeometry scale={1.5} />
+                    <meshBasicMaterial color="mediumpurple"></meshBasicMaterial>
+                </mesh>
+            </group>
 
             {/* Plane / Floor */}
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
