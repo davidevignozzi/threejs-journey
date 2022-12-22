5961acfb70bc3a62b64b14c55b1eec4dcf9d889f
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-22 11:45:26 +0100

    :sparkles: Transform controls

--- react-three-fiber/r3f-drei/src/Experience.js
+++ react-three-fiber/r3f-drei/src/Experience.js
@@ -1,10 +1,15 @@
-import { OrbitControls } from '@react-three/drei';
+import { TransformControls, OrbitControls } from '@react-three/drei';
+import { useRef } from 'react';
 
 export default function Experience() {
+    const cubeRef = useRef();
+
     return (
         <>
             <OrbitControls
-            // is set with enabledDamping: true
+                // is set with enabledDamping: true
+
+                makeDefault // to resolve conflict with TransformControls
             />
             <directionalLight position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
@@ -14,10 +19,24 @@ export default function Experience() {
                 <meshStandardMaterial color="orange" />
             </mesh>
 
-            <mesh position-x={2} scale={1.5}>
+            {/* 
+                <TransformControls position-x={2}>
+                    <mesh scale={1.5}>
+                        <boxGeometry />
+                        <meshStandardMaterial color="mediumpurple" />
+                    </mesh>
+                </TransformControls>
+            */}
+
+            <mesh ref={cubeRef} position-x={2} scale={1.5}>
                 <boxGeometry />
                 <meshStandardMaterial color="mediumpurple" />
             </mesh>
+            <TransformControls
+                object={cubeRef}
+                // mode="rotate"
+                mode="translate"
+            />
 
             <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
