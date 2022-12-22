1d6678fcf9930b292d54f955fb747aa3015b81dc
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-22 10:21:24 +0100

    :sparkles: Camera animations => rotation

--- react-three-fiber/r3f-first-application/src/index.js
+++ react-three-fiber/r3f-first-application/src/index.js
@@ -5,8 +5,27 @@ import Experience from './Experience';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
+// const cameraSettings = {
+//     fov: 45,
+//     zoom: 100,
+//     near: 0.1,
+//     far: 200,
+//     position: [3, 2, 6]
+// };
+
 root.render(
-    <Canvas>
+    <Canvas
+        // orthographic
+        camera={
+            {
+                fov: 45,
+                // zoom: 100,
+                near: 0.1,
+                far: 200,
+                position: [3, 2, 6]
+            }
+            // cameraSettings
+        }>
         <Experience />
     </Canvas>
 );
