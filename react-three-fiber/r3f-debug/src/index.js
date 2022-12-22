d7bc65c52ebcf82913f5ec7672d55f2edc47439e
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-22 16:10:42 +0100

    :adhesive_bandage: Leva Configurations

--- react-three-fiber/r3f-debug/src/index.js
+++ react-three-fiber/r3f-debug/src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
 import Experience from './Experience.js';
 import { StrictMode } from 'react';
+import { Leva } from 'leva';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
     <StrictMode>
+        <Leva collapsed />
         <Canvas
             camera={{
                 fov: 45,
