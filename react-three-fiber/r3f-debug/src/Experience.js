1c149bb95b38a5acd8bc94082601378a3c0c3892
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-22 16:04:09 +0100

    :sparkles: Debug twicks

--- react-three-fiber/r3f-debug/src/Experience.js
+++ react-three-fiber/r3f-debug/src/Experience.js
@@ -1,6 +1,25 @@
 import { OrbitControls } from '@react-three/drei';
+import { useControls, button } from 'leva';
 
 export default function Experience() {
+    const { position, color, visible } = useControls('sphere', {
+        position: {
+            value: {
+                x: -2,
+                y: 0
+                // z: 0
+            },
+            step: 0.01,
+            joystick: 'invertY'
+        },
+        color: '#ff0000', // rgb-'string'-hsl-hsla-{r:0, g:0, b:0, a:1}
+        visible: true,
+        clickMe: button(() => {
+            console.log('cliccked');
+        }),
+        choice: { options: ['a', 'b', 'c'] }
+    });
+
     return (
         <>
             <OrbitControls makeDefault />
@@ -8,9 +27,9 @@ export default function Experience() {
             <directionalLight position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
 
-            <mesh position-x={-2}>
+            <mesh visible={visible} position={[position.x, position.y, 0]}>
                 <sphereGeometry />
-                <meshStandardMaterial color="orange" />
+                <meshStandardMaterial color={color} />
             </mesh>
 
             <mesh position-x={2} scale={1.5}>
