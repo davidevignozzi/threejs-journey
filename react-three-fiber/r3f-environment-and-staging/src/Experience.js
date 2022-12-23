51d2c0194fec0d9f066ee1f3a2a108fc565aafef
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-23 10:10:16 +0100

    :sparkles: R3F Activate Shadows

--- react-three-fiber/r3f-environment-and-staging/src/Experience.js
+++ react-three-fiber/r3f-environment-and-staging/src/Experience.js
@@ -32,20 +32,25 @@ export default function Experience() {
             {/* 
                 Lights
             */}
-            <directionalLight ref={directionalLightRef} position={[1, 2, 3]} intensity={1.5} />
+            <directionalLight
+                ref={directionalLightRef}
+                castShadow
+                position={[1, 2, 3]}
+                intensity={1.5}
+            />
             <ambientLight intensity={0.5} />
 
-            <mesh position-x={-2}>
+            <mesh castShadow position-x={-2}>
                 <sphereGeometry />
                 <meshStandardMaterial color="orange" />
             </mesh>
 
-            <mesh ref={cube} position-x={2} scale={1.5}>
+            <mesh ref={cube} castShadow position-x={2} scale={1.5}>
                 <boxGeometry />
                 <meshStandardMaterial color="mediumpurple" />
             </mesh>
 
-            <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+            <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
                 <meshStandardMaterial color="greenyellow" />
             </mesh>
