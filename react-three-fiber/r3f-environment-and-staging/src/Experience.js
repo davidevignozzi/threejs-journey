fa0e97eec89974745156695576e82d185a5ebb53
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-23 10:06:28 +0100

    :sparkles: R3F Lights Helpers

--- react-three-fiber/r3f-environment-and-staging/src/Experience.js
+++ react-three-fiber/r3f-environment-and-staging/src/Experience.js
@@ -1,10 +1,20 @@
 import { useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { useHelper, OrbitControls } from '@react-three/drei';
 import { useRef } from 'react';
 import { Perf } from 'r3f-perf';
+import * as THREE from 'three';
 
 export default function Experience() {
     const cube = useRef();
+    const directionalLightRef = useRef();
+
+    /**
+     * Lights Helpers
+     */
+    // 1th parameter is the ref
+    // 2nd parameter is the helper class from THREE
+    // 3rd parameter is the size of the helper
+    useHelper(directionalLightRef, THREE.DirectionalLightHelper, 1);
 
     useFrame((state, delta) => {
         cube.current.rotation.y += delta * 0.2;
@@ -13,13 +23,16 @@ export default function Experience() {
     return (
         <>
             {/* Background with R3F */}
-            <color args={['#ff0000']} attach="background" />
+            <color args={['ivory']} attach="background" />
 
             <Perf position="top-left" />
 
             <OrbitControls makeDefault />
 
-            <directionalLight position={[1, 2, 3]} intensity={1.5} />
+            {/* 
+                Lights
+            */}
+            <directionalLight ref={directionalLightRef} position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
 
             <mesh position-x={-2}>
