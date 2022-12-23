77e4b344dfb591ebc80088f4ffb172edcfca8418
Davide Vignozzi <davide.vignozzi@gmail.com>
2022-12-23 12:47:07 +0100

    :sparkles: Environment Map HDR
    git push

--- react-three-fiber/r3f-environment-and-staging/src/Experience.js
+++ react-three-fiber/r3f-environment-and-staging/src/Experience.js
@@ -61,14 +61,17 @@ export default function Experience() {
         <>
             <Environment
                 background
-                files={[
-                    './environmentMaps/2/px.jpg',
-                    './environmentMaps/2/nx.jpg',
-                    './environmentMaps/2/py.jpg',
-                    './environmentMaps/2/ny.jpg',
-                    './environmentMaps/2/pz.jpg',
-                    './environmentMaps/2/nz.jpg'
-                ]}
+                files={
+                    './environmentMaps/the_sky_is_on_fire_2k.hdr'
+                    // [
+                    //     './environmentMaps/2/px.jpg',
+                    //     './environmentMaps/2/nx.jpg',
+                    //     './environmentMaps/2/py.jpg',
+                    //     './environmentMaps/2/ny.jpg',
+                    //     './environmentMaps/2/pz.jpg',
+                    //     './environmentMaps/2/nz.jpg'
+                    // ]
+                }
             />
             {/* Bake Shadows */}
             {/* <BakeShadows /> */}
