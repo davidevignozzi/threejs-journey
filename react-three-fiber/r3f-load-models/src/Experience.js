0ca16cd081a52d9caf30746d0ba1be1b64194e27
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-03 10:16:08 +0100

    :bug: Placeholder in lazy loading

--- react-three-fiber/r3f-load-models/src/Experience.js
+++ react-three-fiber/r3f-load-models/src/Experience.js
@@ -42,7 +42,14 @@ export default function Experience() {
             */}
 
             {/* Lazy Loading */}
-            <Suspense>
+            <Suspense
+                fallback={
+                    <mesh position-y={0.5} scale={[2, 3, 2]}>
+                        <boxGeometry args={[1, 1, 1, 2, 2, 2]} />
+                        <meshBasicMaterial wireframe color="red" />
+                    </mesh>
+                }
+            >
                 <Model />
             </Suspense>
         </>
