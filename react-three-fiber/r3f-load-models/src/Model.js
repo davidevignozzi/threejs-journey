e53c88eeadeb812a232a2f33fef1f8803e10f34d
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-03 10:30:15 +0100

    :sparkles: Drei useGltf

--- react-three-fiber/r3f-load-models/src/Model.js
+++ react-three-fiber/r3f-load-models/src/Model.js
@@ -1,6 +1,7 @@
-import { useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+// import { useLoader } from '@react-three/fiber';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+// import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
+import { useGLTF } from '@react-three/drei';
 
 const Model = () => {
     // FlightHelmet
@@ -12,11 +13,14 @@ const Model = () => {
     // });
 
     // Hamburger
-    const model = useLoader(GLTFLoader, './hamburger-draco.glb', (loader) => {
-        const dracoLoader = new DRACOLoader();
-        dracoLoader.setDecoderPath('./draco/');
-        loader.setDRACOLoader(dracoLoader);
-    });
+    // const model = useLoader(GLTFLoader, './hamburger-draco.glb', (loader) => {
+    //     const dracoLoader = new DRACOLoader();
+    //     dracoLoader.setDecoderPath('./draco/');
+    //     loader.setDRACOLoader(dracoLoader);
+    // });
+
+    const model = useGLTF('./hamburger.glb');
+
     return <primitive object={model.scene} scale={0.3} position-y={-1} />;
 };
 
