85a8fb23edbe2efd0e6461f2269e7f5e1670a3de
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-03 11:37:41 +0100

    :bug: Fix Shadow Acne

--- react-three-fiber/r3f-load-models/src/Experience.js
+++ react-three-fiber/r3f-load-models/src/Experience.js
@@ -29,7 +29,12 @@ export default function Experience() {
 
             <OrbitControls makeDefault />
 
-            <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
+            <directionalLight
+                castShadow
+                position={[1, 2, 3]}
+                intensity={1.5}
+                shadow-normalBias={0.04}
+            />
             <ambientLight intensity={0.5} />
 
             <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
