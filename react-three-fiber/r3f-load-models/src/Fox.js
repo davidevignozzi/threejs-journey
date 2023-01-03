0c3b32cfe9c37ebbfc0e39fc9844b8f83adf2e3b
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-03 12:06:22 +0100

    :dizzy: Animations Tweek in leva

--- react-three-fiber/r3f-load-models/src/Fox.js
+++ react-three-fiber/r3f-load-models/src/Fox.js
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import { useAnimations, useGLTF } from '@react-three/drei';
+import { useControls } from 'leva';
 
 const Fox = () => {
     const model = useGLTF('./Fox/glTF/Fox.gltf');
@@ -8,15 +9,28 @@ const Fox = () => {
     const animations = useAnimations(model.animations, model.scene);
     // console.log("🚀 ~ file: Fox.js:9 ~ Fox ~ animations", animations)
 
-    useEffect(() => {
-        const action = animations.actions.Run;
-        action.play();
+    const { animationName } = useControls({
+        animationName: { options: animations.names }
+    });
 
-        window.setTimeout(() => {
-            animations.actions.Walk.play();
-            animations.actions.Walk.crossFadeFrom(action, 1);
-        }, 2000);
-    }, []);
+    useEffect(() => {
+        // const action = animations.actions.Run;
+        // action.play();
+
+        // window.setTimeout(() => {
+        //     animations.actions.Walk.play();
+        //     animations.actions.Walk.crossFadeFrom(action, 1);
+        // }, 2000);
+
+        const action = animations.actions[animationName];
+        action.reset().fadeIn(0.5).play();
+
+        return () => {
+            // console.log('dispose');
+
+            action.fadeOut(0.5);
+        };
+    }, [animationName]);
 
     return (
         <primitive object={model.scene} scale={0.02} position={[-2.5, 0, 2.5]} rotation-y={0.3} />
