7d26a662ebe02be719b9334045856a10738f0692
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-03 11:49:03 +0100

    :dizzy: Fox Animation at the start

--- react-three-fiber/r3f-load-models/src/Fox.js
+++ react-three-fiber/r3f-load-models/src/Fox.js
@@ -1,7 +1,18 @@
-import { useGLTF } from '@react-three/drei';
+import { useEffect } from 'react';
+import { useAnimations, useGLTF } from '@react-three/drei';
 
 const Fox = () => {
     const model = useGLTF('./Fox/glTF/Fox.gltf');
+    // console.log('🚀 ~ file: Fox.js:5 ~ Fox ~ model', model);
+
+    const animations = useAnimations(model.animations, model.scene);
+    // console.log("🚀 ~ file: Fox.js:9 ~ Fox ~ animations", animations)
+
+    useEffect(() => {
+        const action = animations.actions.Run;
+        action.play();
+    }, []);
+
     return (
         <primitive object={model.scene} scale={0.02} position={[-2.5, 0, 2.5]} rotation-y={0.3} />
     );
