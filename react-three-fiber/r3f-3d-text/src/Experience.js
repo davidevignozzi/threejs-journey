ff1d6bcffe9e774cdffe385de023d9ce13c6299b
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-04 11:22:06 +0100

    :sparkles: 3D Text

--- react-three-fiber/r3f-3d-text/src/Experience.js
+++ react-three-fiber/r3f-3d-text/src/Experience.js
@@ -1,17 +1,30 @@
-import { OrbitControls } from '@react-three/drei';
+import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
 
 export default function Experience() {
+    const [matcapTexture] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256);
     return (
         <>
             <Perf position="top-left" />
 
             <OrbitControls makeDefault />
 
-            <mesh scale={1.5}>
-                <boxGeometry />
-                <meshNormalMaterial />
-            </mesh>
+            <Center>
+                <Text3D
+                    font={'./fonts/helvetiker_regular.typeface.json'}
+                    size={0.75}
+                    height={0.2}
+                    curveSegments={12}
+                    bevelEnabled
+                    bevelThickness={0.02}
+                    bevelSize={0.02}
+                    bevelOffset={0}
+                    bevelSegments={5}
+                >
+                    Hello R3F
+                    <meshMatcapMaterial matcap={matcapTexture} />
+                </Text3D>
+            </Center>
         </>
     );
 }
