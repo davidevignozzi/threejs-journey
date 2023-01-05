f52764dbc04d02e994fbbce93700722270a91177
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-05 10:07:55 +0100

    :sparkles: Background Dark

--- react-three-fiber/r3f-portal-scene/src/Experience.js
+++ react-three-fiber/r3f-portal-scene/src/Experience.js
@@ -3,6 +3,8 @@ import { OrbitControls } from '@react-three/drei';
 export default function Experience() {
     return (
         <>
+            <color args={['#201919']} attach="background" />
+
             <OrbitControls makeDefault />
 
             <mesh scale={1.5}>
