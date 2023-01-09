2fbaf5614232eadd9b49d1107fc8ada280c95ceb
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-09 12:15:43 +0100

    :sparkles: DepthOfField

--- react-three-fiber/r3f-post-processing/src/Experience.js
+++ react-three-fiber/r3f-post-processing/src/Experience.js
@@ -1,26 +1,46 @@
 import { OrbitControls } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
-import { EffectComposer, Vignette, Glitch, Noise, Bloom } from '@react-three/postprocessing';
+import {
+    EffectComposer,
+    Vignette,
+    Glitch,
+    Noise,
+    Bloom,
+    DepthOfField
+} from '@react-three/postprocessing';
 import { BlendFunction, GlitchMode } from 'postprocessing';
 
 export default function Experience() {
     return (
         <>
-            <color args={['#101010']} attach="background" />
+            <color args={['#ffffff']} attach="background" />
             <EffectComposer>
-                {/* <Vignette offset={0.3} darkness={0.9} blendFunction={BlendFunction.NORMAL} /> */}
-                {/* <Glitch
-                    delay={[0.5, 1]}
-                    duration={[0.1, 0.3]}
-                    strength={[0.2, 0.4]}
-                    mode={GlitchMode.SPORADIC}
-                /> */}
-                {/* <Noise premultiply blendFunction={BlendFunction.SOFT_LIGHT} /> */}
-                <Bloom
-                    mipmapBlur
-                    intensity={0.5}
-                    // luminanceThreshold={0}
-                />
+                {/* 
+                    <Vignette offset={0.3} darkness={0.9} blendFunction={BlendFunction.NORMAL} /> 
+                */}
+
+                {/* 
+                    <Glitch
+                        delay={[0.5, 1]}
+                        duration={[0.1, 0.3]}
+                        strength={[0.2, 0.4]}
+                        mode={GlitchMode.SPORADIC}
+                    /> 
+                */}
+
+                {/* 
+                    <Noise premultiply blendFunction={BlendFunction.SOFT_LIGHT} /> 
+                */}
+
+                {/*
+                    <Bloom
+                        mipmapBlur
+                        intensity={0.5}
+                        // luminanceThreshold={0}
+                    />
+                */}
+
+                <DepthOfField focusDistance={0.025} focalLength={0.025} bokehScale={6} />
             </EffectComposer>
 
             <Perf position="top-left" />
@@ -44,7 +64,8 @@ export default function Experience() {
                     emissiveIntensity={4}
                     toneMapped={false}
                 /> */}
-                <meshBasicMaterial color={[1.5, 1, 4]} toneMapped={false} />
+                {/* <meshBasicMaterial color={[1.5, 1, 4]} toneMapped={false} /> */}
+                <meshStandardMaterial color="mediumpurple" />
             </mesh>
 
             <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
