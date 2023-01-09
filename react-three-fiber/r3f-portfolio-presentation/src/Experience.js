38e3caa3c448ee307d1a7c6dac695b5e9afb2c71
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-09 16:03:50 +0100

    :bug: Change import type

--- react-three-fiber/r3f-portfolio-presentation/src/Experience.js
+++ react-three-fiber/r3f-portfolio-presentation/src/Experience.js
@@ -1,7 +1,17 @@
-import { PresentationControls, Environment, Float, ContactShadows } from '@react-three/drei';
-import Laptop from './Laptop';
+import {
+    useGLTF,
+    PresentationControls,
+    Environment,
+    Float,
+    Html,
+    ContactShadows
+} from '@react-three/drei';
 
 export default function Experience() {
+    const laptop = useGLTF(
+        'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
+    );
+
     return (
         <>
             <color args={['#ffffff']} attach="background" />
@@ -17,7 +27,7 @@ export default function Experience() {
                 snap={{ mass: 2, tension: 400 }}
             >
                 <Float rotationIntensity={0.4}>
-                    <Laptop position-y={-1.2} />
+                    <primitive object={laptop.scene} position-y={-1.2} />
                 </Float>
             </PresentationControls>
 
