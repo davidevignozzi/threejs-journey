bffee5d660735fa80dc0ded418a199e43f9cc4fa
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-09 11:02:30 +0100

    :sparkles: Mouse Event on complex objects

--- react-three-fiber/r3f-mouse-events/src/Experience.js
+++ react-three-fiber/r3f-mouse-events/src/Experience.js
@@ -1,9 +1,10 @@
 import { useFrame } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, useGLTF } from '@react-three/drei';
 import { useRef } from 'react';
 
 export default function Experience() {
     const cube = useRef();
+    const hamburger = useGLTF('./hamburger.glb');
 
     const eventHandler = (event) => {
         // console.log('click');
@@ -79,6 +80,16 @@ export default function Experience() {
                 <planeGeometry />
                 <meshStandardMaterial color="greenyellow" />
             </mesh>
+
+            <primitive
+                object={hamburger.scene}
+                scale={0.25}
+                position-y={0.5}
+                onClick={(event) => {
+                    console.log(event.object.name);
+                    event.stopPropagation();
+                }}
+            />
         </>
     );
 }
