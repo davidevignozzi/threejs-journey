193a4f3d0d30ee25c36f39e2781cea58d187eddc
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-09 15:13:42 +0100

    :tada: R3F Portfolio Presentation

---
+++ react-three-fiber/r3f-portfolio-presentation/src/Experience.js
@@ -0,0 +1,14 @@
+import { OrbitControls } from '@react-three/drei';
+
+export default function Experience() {
+    return (
+        <>
+            <OrbitControls makeDefault />
+
+            <mesh>
+                <boxGeometry />
+                <meshNormalMaterial />
+            </mesh>
+        </>
+    );
+}
