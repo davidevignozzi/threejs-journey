d471f532fc4945cc9c821cd3c0d0d5ef17a3e5f9
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-09 17:29:30 +0100

    :art: Screen Light Color

--- react-three-fiber/r3f-portfolio-presentation/src/Experience.js
+++ react-three-fiber/r3f-portfolio-presentation/src/Experience.js
@@ -34,7 +34,7 @@ export default function Experience() {
                         width={2.5}
                         height={1.65}
                         intensity={65}
-                        color={'#FFD7C6'}
+                        color={'#faa582'}
                         rotation={[0.1, Math.PI, 0]}
                         position={[0, 0.55, -1.15]}
                     />
