2b8b5e13bfb725cfc114e4f0e32af3ae21721bbc
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-09 11:53:07 +0100

    :sparkles: Glitch Effect

--- react-three-fiber/r3f-post-processing/src/Experience.js
+++ react-three-fiber/r3f-post-processing/src/Experience.js
@@ -1,14 +1,20 @@
 import { OrbitControls } from '@react-three/drei';
 import { Perf } from 'r3f-perf';
-import { EffectComposer, Vignette } from '@react-three/postprocessing';
-import { BlendFunction } from 'postprocessing';
+import { EffectComposer, Vignette, Glitch } from '@react-three/postprocessing';
+import { BlendFunction, GlitchMode } from 'postprocessing';
 
 export default function Experience() {
     return (
         <>
             <color args={['#ffffff']} attach="background" />
             <EffectComposer>
-                <Vignette offset={0.3} darkness={0.9} blendFunction={BlendFunction.NORMAL} />
+                {/* <Vignette offset={0.3} darkness={0.9} blendFunction={BlendFunction.NORMAL} /> */}
+                <Glitch
+                    delay={[0.5, 1]}
+                    duration={[0.1, 0.3]}
+                    strength={[0.2, 0.4]}
+                    mode={GlitchMode.SPORADIC}
+                />
             </EffectComposer>
 
             <Perf position="top-left" />
