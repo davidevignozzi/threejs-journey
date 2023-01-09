c6db8a858bc5e69f6eca3c77fd557f94e9b8e89a
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-09 12:25:41 +0100

    :sparkles: SSR

--- react-three-fiber/r3f-post-processing/src/Experience.js
+++ react-three-fiber/r3f-post-processing/src/Experience.js
@@ -6,11 +6,44 @@ import {
     Glitch,
     Noise,
     Bloom,
-    DepthOfField
+    DepthOfField,
+    SSR
 } from '@react-three/postprocessing';
 import { BlendFunction, GlitchMode } from 'postprocessing';
+import { useControls } from 'leva';
 
 export default function Experience() {
+    const ssrProps = useControls('SSR Effect', {
+        temporalResolve: true,
+        STRETCH_MISSED_RAYS: true,
+        USE_MRT: true,
+        USE_NORMALMAP: true,
+        USE_ROUGHNESSMAP: true,
+        ENABLE_JITTERING: true,
+        ENABLE_BLUR: true,
+        temporalResolveMix: { value: 0.9, min: 0, max: 1 },
+        temporalResolveCorrectionMix: { value: 0.25, min: 0, max: 1 },
+        maxSamples: { value: 0, min: 0, max: 1 },
+        resolutionScale: { value: 1, min: 0, max: 1 },
+        blurMix: { value: 0.5, min: 0, max: 1 },
+        blurKernelSize: { value: 8, min: 0, max: 8 },
+        blurSharpness: { value: 0.5, min: 0, max: 1 },
+        rayStep: { value: 0.3, min: 0, max: 1 },
+        intensity: { value: 1, min: 0, max: 5 },
+        maxRoughness: { value: 0.1, min: 0, max: 1 },
+        jitter: { value: 0.7, min: 0, max: 5 },
+        jitterSpread: { value: 0.45, min: 0, max: 1 },
+        jitterRough: { value: 0.1, min: 0, max: 1 },
+        roughnessFadeOut: { value: 1, min: 0, max: 1 },
+        rayFadeOut: { value: 0, min: 0, max: 1 },
+        MAX_STEPS: { value: 20, min: 0, max: 20 },
+        NUM_BINARY_SEARCH_STEPS: { value: 5, min: 0, max: 10 },
+        maxDepthDifference: { value: 3, min: 0, max: 10 },
+        maxDepth: { value: 1, min: 0, max: 1 },
+        thickness: { value: 10, min: 0, max: 10 },
+        ior: { value: 1.45, min: 0, max: 2 }
+    });
+
     return (
         <>
             <color args={['#ffffff']} attach="background" />
@@ -40,7 +73,11 @@ export default function Experience() {
                     />
                 */}
 
-                <DepthOfField focusDistance={0.025} focalLength={0.025} bokehScale={6} />
+                {/* 
+                    <DepthOfField focusDistance={0.025} focalLength={0.025} bokehScale={6} />
+                */}
+
+                <SSR {...ssrProps} />
             </EffectComposer>
 
             <Perf position="top-left" />
@@ -70,7 +107,7 @@ export default function Experience() {
 
             <mesh receiveShadow position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
                 <planeGeometry />
-                <meshStandardMaterial color="greenyellow" />
+                <meshStandardMaterial color="greenyellow" metalness={0} roughness={0} />
             </mesh>
         </>
     );
