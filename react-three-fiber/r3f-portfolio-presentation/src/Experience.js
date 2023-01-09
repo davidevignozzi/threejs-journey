353131dfa485030f719259c2dd522fb4ab5e4593
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-09 15:41:30 +0100

    :sparkles: PresentationControls

--- react-three-fiber/r3f-portfolio-presentation/src/Experience.js
+++ react-three-fiber/r3f-portfolio-presentation/src/Experience.js
@@ -1,4 +1,4 @@
-import { OrbitControls, Environment, Float } from '@react-three/drei';
+import { PresentationControls, Environment, Float } from '@react-three/drei';
 import Laptop from './Laptop';
 
 export default function Experience() {
@@ -7,11 +7,19 @@ export default function Experience() {
             <color args={['#333333']} attach="background" />
 
             <Environment preset="city" />
-            <OrbitControls makeDefault />
 
-            <Float rotationIntensity={0.4}>
-                <Laptop position-y={-1.2} />
-            </Float>
+            <PresentationControls
+                global
+                rotation={[0.13, 0.1, 0]}
+                polar={[-0.4, 0.2]}
+                azimuth={[-1, 0.75]}
+                config={{ mass: 2, tension: 400 }}
+                snap={{ mass: 2, tension: 400 }}
+            >
+                <Float rotationIntensity={0.4}>
+                    <Laptop position-y={-1.2} />
+                </Float>
+            </PresentationControls>
         </>
     );
 }
