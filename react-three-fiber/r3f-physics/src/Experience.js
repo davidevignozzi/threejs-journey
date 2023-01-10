5b09ddb4a7eb4e64e76865af505f3102695b0f5c
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-10 15:21:00 +0100

    :sparkles: BallCollider

--- react-three-fiber/r3f-physics/src/Experience.js
+++ react-three-fiber/r3f-physics/src/Experience.js
@@ -1,5 +1,5 @@
 import { OrbitControls } from '@react-three/drei';
-import { Physics, RigidBody, CuboidCollider, Debug } from '@react-three/rapier';
+import { Physics, RigidBody, CuboidCollider, BallCollider, Debug } from '@react-three/rapier';
 import { Perf } from 'r3f-perf';
 
 export default function Experience() {
@@ -25,12 +25,7 @@ export default function Experience() {
 
                 {/* Donut */}
                 <RigidBody colliders={false} position={[0, 1, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
-                    <CuboidCollider args={[1.5, 1.5, 0.5]} />
-                    <CuboidCollider
-                        args={[0.25, 1, 0.25]}
-                        position={[0, 0, 1]}
-                        rotation={[-Math.PI * 0.35, 0, 0]}
-                    />
+                    <BallCollider args={[1.5]} />
                     <mesh castShadow>
                         <torusGeometry args={[1, 0.5, 16, 42]} />
                         <meshStandardMaterial color="midiumpurple" />
