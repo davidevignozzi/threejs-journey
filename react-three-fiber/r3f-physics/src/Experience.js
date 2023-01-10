8a6dfc8ab296cd9ccfc962a2bf29bd2bdfd9a0bd
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-10 14:53:38 +0100

    :sparkles: Implementing Physics

--- react-three-fiber/r3f-physics/src/Experience.js
+++ react-three-fiber/r3f-physics/src/Experience.js
@@ -1,5 +1,5 @@
 import { OrbitControls } from '@react-three/drei';
-import { Physics } from '@react-three/rapier';
+import { Physics, RigidBody } from '@react-three/rapier';
 import { Perf } from 'r3f-perf';
 
 export default function Experience() {
@@ -12,20 +12,29 @@ export default function Experience() {
             <directionalLight castShadow position={[1, 2, 3]} intensity={1.5} />
             <ambientLight intensity={0.5} />
 
-            <mesh castShadow position={[-2, 2, 0]}>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-            </mesh>
+            <Physics>
+                {/* Sphere */}
+                <RigidBody>
+                    <mesh castShadow position={[-2, 2, 0]}>
+                        <sphereGeometry />
+                        <meshStandardMaterial color="orange" />
+                    </mesh>
+                </RigidBody>
 
-            <mesh castShadow position={[2, 2, 0]}>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
+                {/* Cube */}
+                <mesh castShadow position={[2, 2, 0]}>
+                    <boxGeometry />
+                    <meshStandardMaterial color="mediumpurple" />
+                </mesh>
 
-            <mesh receiveShadow position-y={-1.25}>
-                <boxGeometry args={[10, 0.5, 10]} />
-                <meshStandardMaterial color="greenyellow" />
-            </mesh>
+                {/* Floor */}
+                <RigidBody type="fixed">
+                    <mesh receiveShadow position-y={-1.25}>
+                        <boxGeometry args={[10, 0.5, 10]} />
+                        <meshStandardMaterial color="greenyellow" />
+                    </mesh>
+                </RigidBody>
+            </Physics>
         </>
     );
 }
