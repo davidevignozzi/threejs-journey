b0eb1c2f522f7792911363e9f074d7215225f083
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-10 15:30:58 +0100

    :sparkles: cubeJump

--- react-three-fiber/r3f-physics/src/Experience.js
+++ react-three-fiber/r3f-physics/src/Experience.js
@@ -1,8 +1,16 @@
+import { useRef } from 'react';
 import { OrbitControls } from '@react-three/drei';
 import { Physics, RigidBody, CuboidCollider, BallCollider, Debug } from '@react-three/rapier';
 import { Perf } from 'r3f-perf';
 
 export default function Experience() {
+    const cube = useRef();
+
+    const cubeJump = () => {
+        // console.log(cube.current);
+        cube.current.applyImpulse({ x: 0, y: 5, z: 0 });
+        cube.current.applyTorqueImpulse({ x: 0, y: 1, z: 0 });
+    };
     return (
         <>
             <Perf position="top-left" />
@@ -17,18 +25,28 @@ export default function Experience() {
 
                 {/* Sphere */}
                 <RigidBody colliders="ball">
-                    <mesh castShadow position={[0, 4, 0]}>
+                    <mesh castShadow position={[-1.5, 2, 0]}>
                         <sphereGeometry />
                         <meshStandardMaterial color="orange" />
                     </mesh>
                 </RigidBody>
 
                 {/* Donut */}
-                <RigidBody colliders={false} position={[0, 1, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
-                    <BallCollider args={[1.5]} />
-                    <mesh castShadow>
-                        <torusGeometry args={[1, 0.5, 16, 42]} />
-                        <meshStandardMaterial color="midiumpurple" />
+                {/* 
+                    <RigidBody colliders={false} position={[0, 1, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
+                        <BallCollider args={[1.5]} />
+                        <mesh castShadow>
+                            <torusGeometry args={[1, 0.5, 16, 42]} />
+                            <meshStandardMaterial color="midiumpurple" />
+                        </mesh>
+                    </RigidBody>
+                */}
+
+                {/* Cube */}
+                <RigidBody ref={cube} position={[1.5, 2, 0]}>
+                    <mesh castShadow onClick={cubeJump}>
+                        <boxGeometry />
+                        <meshStandardMaterial color="mediumpurple" />
                     </mesh>
                 </RigidBody>
 
