79f6a0035c19edc0016002adc7946a5afa1a306b
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-10 10:09:20 +0100

    :children_crossing: preload

--- react-three-fiber/r3f-portfolio-presentation/src/Experience.js
+++ react-three-fiber/r3f-portfolio-presentation/src/Experience.js
@@ -7,6 +7,7 @@ import {
     Text,
     ContactShadows
 } from '@react-three/drei';
+import { Suspense } from 'react';
 import image from './Images/ComingSoon.png';
 
 export default function Experience() {
@@ -20,40 +21,41 @@ export default function Experience() {
 
             <Environment preset="city" />
 
-            <PresentationControls
-                global
-                rotation={[0.13, 0.1, 0]}
-                polar={[-0.4, 0.2]}
-                azimuth={[-1, 0.75]}
-                config={{ mass: 2, tension: 400 }}
-                snap={{ mass: 2, tension: 400 }}
-            >
-                <Float rotationIntensity={0.4}>
-                    {/* Screen Light */}
-                    <rectAreaLight
-                        width={2.5}
-                        height={1.65}
-                        intensity={65}
-                        color={'#faa582'}
-                        rotation={[0.1, Math.PI, 0]}
-                        position={[0.75, 0.55, -1.15]}
-                    />
+            <Suspense fallback={null}>
+                <PresentationControls
+                    global
+                    rotation={[0.13, 0.1, 0]}
+                    polar={[-0.4, 0.2]}
+                    azimuth={[-1, 0.75]}
+                    config={{ mass: 2, tension: 400 }}
+                    snap={{ mass: 2, tension: 400 }}
+                >
+                    <Float rotationIntensity={0.4}>
+                        {/* Screen Light */}
+                        <rectAreaLight
+                            width={2.5}
+                            height={1.65}
+                            intensity={65}
+                            color={'#faa582'}
+                            rotation={[0.1, Math.PI, 0]}
+                            position={[0.75, 0.55, -1.15]}
+                        />
 
-                    {/* Laptop */}
-                    <primitive object={laptop.scene} position-x={0.75} position-y={-1.2}>
-                        {/* Image with the actual website */}
-                        <Html
-                            transform
-                            wrapperClass="htmlScreen"
-                            distanceFactor={1.17}
-                            position={[0, 1.56, -1.4]}
-                            rotation-x={-0.256}
-                        >
-                            <img src={image} />
-                        </Html>
-                    </primitive>
+                        {/* Laptop */}
+                        <primitive object={laptop.scene} position-x={0.75} position-y={-1.2}>
+                            {/* Image with the actual website */}
+                            <Html
+                                transform
+                                wrapperClass="htmlScreen"
+                                distanceFactor={1.17}
+                                position={[0, 1.56, -1.4]}
+                                rotation-x={-0.256}
+                            >
+                                <img src={image} />
+                            </Html>
+                        </primitive>
 
-                    {/* 
+                        {/* 
                         <Text
                             font="./bangers-v20-latin-regular.woff"
                             fontSize={1}
@@ -65,10 +67,15 @@ export default function Experience() {
                             DAVIDE VIGNOZZI
                         </Text> 
                     */}
-                </Float>
-            </PresentationControls>
+                    </Float>
+                </PresentationControls>
+            </Suspense>
 
             <ContactShadows position-y={-1.4} opacity={0.4} scale={5} blur={2.4} />
         </>
     );
 }
+
+useGLTF.preload(
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
+);
