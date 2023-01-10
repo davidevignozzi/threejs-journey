a1560c1eadf3c2ddcf567693d119cda09b79bc8e
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-10 15:11:16 +0100

    :sparkles: Trimesh

--- react-three-fiber/r3f-physics/src/Experience.js
+++ react-three-fiber/r3f-physics/src/Experience.js
@@ -24,8 +24,8 @@ export default function Experience() {
                 </RigidBody>
 
                 {/* Donut */}
-                <RigidBody colliders="hull">
-                    <mesh castShadow position={[0, 1, 0]} rotation={[Math.PI * 0.1, 0, 0]}>
+                <RigidBody colliders="trimesh">
+                    <mesh castShadow position={[0, 1, 0]} rotation={[Math.PI * 0.5, 0, 0]}>
                         <torusGeometry args={[1, 0.5, 16, 42]} />
                         <meshStandardMaterial color="midiumpurple" />
                     </mesh>
