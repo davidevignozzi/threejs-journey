d5b00ab36aec98f4e29e1d6fdfdfacbb0f117f73
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-11 14:36:55 +0100

    :sparkles: Collider for the floor
    To not fall

--- react-three-fiber/r3f-jumping-sphere-game/src/Level.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/Level.js
@@ -2,7 +2,7 @@ import { useState, useRef, useMemo } from 'react';
 import * as THREE from 'three';
 import { useFrame } from '@react-three/fiber';
 import { useGLTF } from '@react-three/drei';
-import { RigidBody } from '@react-three/rapier';
+import { RigidBody, CuboidCollider } from '@react-three/rapier';
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
@@ -240,6 +240,14 @@ const Bounds = ({ length = 1 }) => {
                     material={wallMaterial}
                     scale={[4, 1.5, 0.3]}
                     receiveShadow
+                />
+
+                {/* Floor Collider */}
+                <CuboidCollider
+                    args={[2, 0.1, 2 * length]}
+                    position={[0, -0.1, -(length * 2) + 2]}
+                    restitution={0.2}
+                    friction={1}
                 />
             </RigidBody>
         </>
