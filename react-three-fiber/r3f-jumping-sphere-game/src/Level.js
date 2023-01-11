0ba1144bfd49ed16a923b49a7a736fdc44132f56
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-11 12:49:51 +0100

    :sparkles: Created Limbo Trap Block

--- react-three-fiber/r3f-jumping-sphere-game/src/Level.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/Level.js
@@ -76,11 +76,56 @@ const BlockSpinner = ({ position = [0, 0, 0] }) => {
     );
 };
 
+/**
+ *  Limbo Block
+ */
+const BlockLimbo = ({ position = [0, 0, 0] }) => {
+    const obstacle = useRef();
+    const [speed] = useState(() => (Math.random() + 0.2) * (Math.random() < 0.5 ? -1 : 1));
+
+    // spinner animation
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime();
+
+        const y = Math.sin(time) + 1.15;
+        obstacle.current.setNextKinematicTranslation({ x: 0, y: y, z: 0 });
+    });
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor2Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+
+            {/* Spinner Obstacle */}
+            <RigidBody
+                ref={obstacle}
+                type="kinematicPosition"
+                position={[0, 0.3, 0]}
+                restitution={0.2}
+                friction={0}
+            >
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[3.5, 0.3, 0.3]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
+        </group>
+    );
+};
+
 const Level = () => {
     return (
         <>
-            <BlockStart position={[0, 0, 4]} />
-            <BlockSpinner position={[0, 0, 0]} />
+            <BlockStart position={[0, 0, 8]} />
+            <BlockSpinner position={[0, 0, 4]} />
+            <BlockLimbo position={[0, 0, 0]} />
         </>
     );
 };
