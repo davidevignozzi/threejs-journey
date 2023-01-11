6e945f0f50f57e9675f2fa170f07ecb13b15beff
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-11 11:59:45 +0100

    :zap: Instaciate materials out of the component

--- react-three-fiber/r3f-jumping-sphere-game/src/Level.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/Level.js
@@ -2,12 +2,25 @@ import * as THREE from 'three';
 
 const boxGeometry = new THREE.BoxGeometry(1, 1, 1);
 
+/**
+ * Materials
+ */
+THREE.ColorManagement.legacyMode = false;
+const floor1Material = new THREE.MeshStandardMaterial({ color: 'limegreen' });
+const floor2Material = new THREE.MeshStandardMaterial({ color: 'greenyellow' });
+const obstacleMaterial = new THREE.MeshStandardMaterial({ color: 'orangered' });
+const wallMaterial = new THREE.MeshStandardMaterial({ color: 'slategrey' });
+
 const BlockStart = ({ position = [0, 0, 0] }) => {
     return (
         <group position={position}>
-            <mesh geometry={boxGeometry} position={[0, -0.1, 0]} scale={[4, 0.2, 4]} receiveShadow>
-                <meshStandardMaterial color="limegreen" />
-            </mesh>
+            <mesh
+                geometry={boxGeometry}
+                material={floor1Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
         </group>
     );
 };
