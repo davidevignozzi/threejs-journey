29e4a03dfcd57e12d1813b278c9563360f4ec04d
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-11 13:00:08 +0100

    :sparkles: Created and animated Axe Block

--- react-three-fiber/r3f-jumping-sphere-game/src/Level.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/Level.js
@@ -124,12 +124,61 @@ const BlockLimbo = ({ position = [0, 0, 0] }) => {
     );
 };
 
+/**
+ *  Axe Block
+ */
+const BlockAxe = ({ position = [0, 0, 0] }) => {
+    const obstacle = useRef();
+    const [timeOffset] = useState(() => Math.random() * Math.PI * 2);
+
+    // Axe animation
+    useFrame((state) => {
+        const time = state.clock.getElapsedTime();
+
+        const x = Math.sin(time + timeOffset) * 1.25;
+        obstacle.current.setNextKinematicTranslation({
+            x: position[0] + x,
+            y: position[1] + 0.75,
+            z: position[2]
+        });
+    });
+    return (
+        <group position={position}>
+            <mesh
+                geometry={boxGeometry}
+                material={floor2Material}
+                position={[0, -0.1, 0]}
+                scale={[4, 0.2, 4]}
+                receiveShadow
+            />
+
+            {/* Axe Obstacle */}
+            <RigidBody
+                ref={obstacle}
+                type="kinematicPosition"
+                position={[0, 0.3, 0]}
+                restitution={0.2}
+                friction={0}
+            >
+                <mesh
+                    geometry={boxGeometry}
+                    material={obstacleMaterial}
+                    scale={[1.5, 1.5, 0.3]}
+                    castShadow
+                    receiveShadow
+                />
+            </RigidBody>
+        </group>
+    );
+};
+
 const Level = () => {
     return (
         <>
-            <BlockStart position={[0, 0, 8]} />
-            <BlockSpinner position={[0, 0, 4]} />
-            <BlockLimbo position={[0, 0, 0]} />
+            <BlockStart position={[0, 0, 12]} />
+            <BlockSpinner position={[0, 0, 8]} />
+            <BlockLimbo position={[0, 0, 4]} />
+            <BlockAxe position={[0, 0, 0]} />
         </>
     );
 };
