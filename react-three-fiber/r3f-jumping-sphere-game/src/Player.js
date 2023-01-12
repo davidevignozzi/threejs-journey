30fb500a0a784fbe3e14165b7cd09b19efba1a19
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-12 15:31:57 +0100

    :bug: Prevent the multiple jump
    No double jump

--- react-three-fiber/r3f-jumping-sphere-game/src/Player.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/Player.js
@@ -1,15 +1,34 @@
 import { useRef, useEffect } from 'react';
 import { useFrame } from '@react-three/fiber';
 import { useKeyboardControls } from '@react-three/drei';
-import { RigidBody } from '@react-three/rapier';
+import { useRapier, RigidBody } from '@react-three/rapier';
 
 const Player = () => {
     const body = useRef();
     const [subscribeKeys, getKeys] = useKeyboardControls();
 
+    const { rapier, world } = useRapier();
+
+    /**
+     * Jump
+     */
     const jump = () => {
-        // console.log('Jump');
-        body.current.applyImpulse({ x: 0, y: 0.5, z: 0 });
+        // * prevent the multiple jump
+        // cast a ray from the ball to the floor to check the distance
+        const origin = body.current.translation();
+        origin.y -= 0.31;
+        const direction = { x: 0, y: -1, z: 0 };
+        const ray = new rapier.Ray(origin, direction);
+        const rapierWorld = world.raw();
+        const hit = rapierWorld.castRay(
+            ray,
+            10,
+            true // tomake the floor solid
+        );
+
+        if (hit.toi < 0.15) {
+            body.current.applyImpulse({ x: 0, y: 0.5, z: 0 });
+        }
     };
 
     useEffect(() => {
