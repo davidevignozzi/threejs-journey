3684c6ee6778cae220986aa542af37f3a5629aea
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-12 15:06:36 +0100

    :sparkles: Player movements

--- react-three-fiber/r3f-jumping-sphere-game/src/Player.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/Player.js
@@ -1,8 +1,50 @@
+import { useRef } from 'react';
+import { useFrame } from '@react-three/fiber';
+import { useKeyboardControls } from '@react-three/drei';
 import { RigidBody } from '@react-three/rapier';
 
 const Player = () => {
+    const body = useRef();
+    const [subscribeKeys, getKeys] = useKeyboardControls();
+    useFrame((state, delta) => {
+        /**
+         *  KeyboardControls
+         */
+        // const keys = getKeys();
+        const { forward, backward, leftward, rightward } = getKeys();
+
+        const impulse = { x: 0, y: 0, z: 0 };
+        const torque = { x: 0, y: 0, z: 0 };
+
+        const impulseStrength = 0.6 * delta;
+        const torqueStrength = 0.2 * delta;
+
+        if (forward) {
+            impulse.z -= impulseStrength;
+            torque.x -= torqueStrength;
+        }
+
+        if (rightward) {
+            impulse.x += impulseStrength;
+            torque.z -= torqueStrength;
+        }
+
+        if (backward) {
+            impulse.z += impulseStrength;
+            torque.x += torqueStrength;
+        }
+
+        if (leftward) {
+            impulse.x -= impulseStrength;
+            torque.z += torqueStrength;
+        }
+
+        body.current.applyImpulse(impulse);
+        body.current.applyTorqueImpulse(torque);
+    });
+
     return (
-        <RigidBody colliders="ball" restitution={0.2} friction={1} position={(0, 1, 0)}>
+        <RigidBody ref={body} colliders="ball" restitution={0.2} friction={1} position={(0, 1, 0)}>
             <mesh castShadow>
                 <icosahedronGeometry args={[0.3, 1]} />
                 <meshStandardMaterial flatShading color="mediumpurple" />
