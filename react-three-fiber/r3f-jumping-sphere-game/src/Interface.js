e62651840cb92c557621253ca0e28c6fc6f1195c
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-12 16:47:46 +0100

    :sparkles: Controls interface

--- react-three-fiber/r3f-jumping-sphere-game/src/Interface.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/Interface.js
@@ -1,6 +1,13 @@
 import React from 'react';
+import { useKeyboardControls } from '@react-three/drei';
 
 const Interface = () => {
+    const forward = useKeyboardControls((state) => state.forward);
+    const rightward = useKeyboardControls((state) => state.rightward);
+    const backward = useKeyboardControls((state) => state.backward);
+    const leftward = useKeyboardControls((state) => state.leftward);
+    const jump = useKeyboardControls((state) => state.jump);
+
     return (
         <div className="interface">
             {/* Timer */}
@@ -10,6 +17,21 @@ const Interface = () => {
 
             {/* Restart */}
             <div className="restart">restart</div>
+
+            {/* Controls */}
+            <div className="controls">
+                <div className="raw">
+                    <div className={`key ${forward ? 'active' : ''}`}></div>
+                </div>
+                <div className="raw">
+                    <div className={`key ${leftward ? 'active' : ''}`}></div>
+                    <div className={`key ${backward ? 'active' : ''}`}></div>
+                    <div className={`key ${rightward ? 'active' : ''}`}></div>
+                </div>
+                <div className="raw">
+                    <div className={`key large ${jump ? 'active' : ''}`}></div>
+                </div>
+            </div>
         </div>
     );
 };
