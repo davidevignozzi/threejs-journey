826cd4a8d65bf30cdc2398680f070fc4ab956f61
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-12 16:30:30 +0100

    :sparkles: Timer Interface

--- react-three-fiber/r3f-jumping-sphere-game/src/Interface.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/Interface.js
@@ -1,7 +1,14 @@
 import React from 'react';
 
 const Interface = () => {
-    return <div className="interface"></div>;
+    return (
+        <div className="interface">
+            {/* Timer */}
+            <div className="time">
+                <span>0.00</span>
+            </div>
+        </div>
+    );
 };
 
 export default Interface;
