0fda79428dc6a1338038d103032a98edd895f924
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-12 17:25:38 +0100

    :zap: Start with the phases

--- react-three-fiber/r3f-jumping-sphere-game/src/stores/useGame.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/stores/useGame.js
@@ -1,8 +1,35 @@
 import create from 'zustand';
 
-export default create(() => {
+export default create((set) => {
     return {
         // * the block count
-        blocksCount: 3
+        blocksCount: 5,
+
+        /**
+         * Phases
+         */
+        // * First fase
+        phase: 'ready',
+
+        // * When User press a key the phase will change in 'playing'
+        start: () => {
+            set(() => {
+                return { phase: 'playing' };
+            });
+        },
+
+        // * When user press restart return to initial phase
+        restart: () => {
+            set(() => {
+                return { phase: 'ready' };
+            });
+        },
+
+        // * When user end the level the state will be 'ended'
+        end: () => {
+            set(() => {
+                return { phase: 'ended' };
+            });
+        }
     };
 });
