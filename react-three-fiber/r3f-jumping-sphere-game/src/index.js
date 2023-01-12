a7cd31f0b1a41cfe0a15fcf6b690b649a2817c73
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-12 14:49:32 +0100

    :sparkles: KeyboardControls

--- react-three-fiber/r3f-jumping-sphere-game/src/index.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/index.js
@@ -1,20 +1,31 @@
 import './style.css';
 import ReactDOM from 'react-dom/client';
 import { Canvas } from '@react-three/fiber';
+import { KeyboardControls } from '@react-three/drei';
 import Experience from './Experience.js';
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 
 root.render(
-    <Canvas
-        shadows
-        camera={{
-            fov: 45,
-            near: 0.1,
-            far: 200,
-            position: [2.5, 4, 6]
-        }}
+    <KeyboardControls
+        map={[
+            { name: 'forward', keys: ['ArrowUp', 'KeyW'] },
+            { name: 'backward', keys: ['ArrowDown', 'KeyS'] },
+            { name: 'leftward', keys: ['ArrowLeft', 'KeyA'] },
+            { name: 'rightward', keys: ['ArrowRight', 'KeyD'] },
+            { name: 'jump', keys: ['Space'] }
+        ]}
     >
-        <Experience />
-    </Canvas>
+        <Canvas
+            shadows
+            camera={{
+                fov: 45,
+                near: 0.1,
+                far: 200,
+                position: [2.5, 4, 6]
+            }}
+        >
+            <Experience />
+        </Canvas>
+    </KeyboardControls>
 );
