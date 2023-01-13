2767b6e507721cc6c0dc0d0408255d45adc0ef26
Davide Vignozzi <davide.vignozzi@gmail.com>
2023-01-13 10:21:13 +0100

    :adhesive_bandage: little fix => limit change phases

--- react-three-fiber/r3f-jumping-sphere-game/src/stores/useGame.js
+++ react-three-fiber/r3f-jumping-sphere-game/src/stores/useGame.js
@@ -13,22 +13,43 @@ export default create((set) => {
 
         // * When User press a key the phase will change in 'playing'
         start: () => {
-            set(() => {
-                return { phase: 'playing' };
+            set((state) => {
+                if (state.phase === 'ready') {
+                    return { phase: 'playing' };
+                } else {
+                    return {
+                        // have to return something
+                        // empty object
+                    };
+                }
             });
         },
 
         // * When user press restart return to initial phase
         restart: () => {
-            set(() => {
-                return { phase: 'ready' };
+            set((state) => {
+                if (state.phase === 'playing' || state.phase === 'ended') {
+                    return { phase: 'ready' };
+                } else {
+                    return {
+                        // have to return something
+                        // empty object
+                    };
+                }
             });
         },
 
         // * When user end the level the state will be 'ended'
         end: () => {
-            set(() => {
-                return { phase: 'ended' };
+            set((state) => {
+                if (state.phase === 'playing') {
+                    return { phase: 'ended' };
+                } else {
+                    return {
+                        // have to return something
+                        // empty object
+                    };
+                }
             });
         }
     };
