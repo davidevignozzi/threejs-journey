3417d8abbbb2d2c4b35b3637662dad370639029f
Davide Vignozzi <davide.vignozzi@gmail.com>
2024-01-26 15:36:33 +0100

    :truck: R3f 3d text

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,150 +1,77 @@
-import { useFrame } from '@react-three/fiber'
-import { Stage, Lightformer, Environment, Sky, ContactShadows, RandomizedLight, AccumulativeShadows, SoftShadows, BakeShadows, useHelper, OrbitControls } from '@react-three/drei'
-import { useRef } from 'react'
+import { useMatcapTexture, Center, Text3D, OrbitControls } from '@react-three/drei'
 import { Perf } from 'r3f-perf'
+import { useEffect, useRef, useState } from 'react'
+import { useFrame } from '@react-three/fiber'
 import * as THREE from 'three'
-import { useControls } from 'leva'
+
+const torusGeometry = new THREE.TorusGeometry(1, 0.6, 16, 32)
+const material = new THREE.MeshMatcapMaterial()
 
 export default function Experience()
 {
-    const directionalLight = useRef()
-    useHelper(directionalLight, THREE.DirectionalLightHelper, 1)
+    const donuts = useRef([])
 
-    const { color, opacity, blur } = useControls('contact shadows', {
-        color: '#1d8f75',
-        opacity: { value: 0.4, min: 0, max: 1 },
-        blur: { value: 2.8, min: 0, max: 10 },
-    })
+    const [ matcapTexture ] = useMatcapTexture('7B5254_E9DCC7_B19986_C8AC91', 256)
 
-    const { sunPosition } = useControls('sky', {
-        sunPosition: { value: [ 1, 2, 3 ] }
-    })
-
-    const { envMapIntensity, envMapHeight, envMapRadius, envMapScale } = useControls('environment map', {
-        envMapIntensity: { value: 7, min: 0, max: 12 },
-        envMapHeight: { value: 7, min: 0, max: 100 },
-        envMapRadius: { value: 28, min: 10, max: 1000 },
-        envMapScale: { value: 100, min: 10, max: 1000 }
-    })
-
-    const cube = useRef()
-    
     useFrame((state, delta) =>
     {
-        // const time = state.clock.elapsedTime
-        // cube.current.position.x = 2 + Math.sin(time)
-        cube.current.rotation.y += delta * 0.2
+        for(const donut of donuts.current)
+        {
+            donut.rotation.y += delta * 0.2
+        }
     })
 
-    return <>
+    useEffect(() =>
+    {
+        matcapTexture.colorSpace = THREE.SRGBColorSpace
+        matcapTexture.needsUpdate = true
 
-        <color args={ [ 'ivory' ] } attach="background" />
+        material.matcap = matcapTexture
+        material.needsUpdate = true
+    }, [])
+
+    return <>
 
         <Perf position="top-left" />
 
         <OrbitControls makeDefault />
-        
-        {/* <BakeShadows /> */}
-        {/* <SoftShadows size={ 25 } samples={ 10 } focus={ 0 } /> */}
 
-        {/* <Environment
-            background
-            // files={ [
-            //     './environmentMaps/2/px.jpg',
-            //     './environmentMaps/2/nx.jpg',
-            //     './environmentMaps/2/py.jpg',
-            //     './environmentMaps/2/ny.jpg',
-            //     './environmentMaps/2/pz.jpg',
-            //     './environmentMaps/2/nz.jpg',
-            // ] }
-            // files="./environmentMaps/the_sky_is_on_fire_2k.hdr"
-            preset="sunset"
-            resolution={ 32 }
-            ground={ {
-                height: envMapHeight,
-                radius: envMapRadius,
-                scale: envMapScale
-            } }
-        >
-        </Environment> */}
+        <Center>
+            <Text3D
+                material={ material }
+                font="./fonts/helvetiker_regular.typeface.json"
+                size={ 0.75 }
+                height={ 0.2 }
+                curveSegments={ 12 }
+                bevelEnabled
+                bevelThickness={ 0.02 }
+                bevelSize={ 0.02 }
+                bevelOffset={ 0 }
+                bevelSegments={ 5 }
+            >
+                HELLO R3F
+            </Text3D>
+        </Center>
 
-        {/* <AccumulativeShadows
-            position={ [ 0, - 0.99, 0 ] }
-            scale={ 10 }
-            color="#316d39"
-            opacity={ 0.8 }
-            frames={ Infinity }
-            temporal
-            blend={ 100 }
-        >
-            <RandomizedLight
-                amount={ 8 }
-                radius={ 1 }
-                ambient={ 0.5 }
-                intensity={ 3 }
-                position={ [ 1, 2, 3 ] }
-                bias={ 0.001 }
+        { [...Array(100)].map((value, index) =>
+            <mesh
+                ref={ (element) => donuts.current[index] = element }
+                key={ index }
+                geometry={ torusGeometry }
+                material={ material }
+                position={ [
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10,
+                    (Math.random() - 0.5) * 10
+                ] }
+                scale={ 0.2 + Math.random() * 0.2 }
+                rotation={ [
+                    Math.random() * Math.PI,
+                    Math.random() * Math.PI,
+                    0
+                ] }
             />
-        </AccumulativeShadows> */}
-
-        {/* <Sky sunPosition={ sunPosition } /> */}
-
-        {/* <directionalLight
-            ref={ directionalLight }
-            position={ sunPosition }
-            intensity={ 4.5 }
-            castShadow
-            shadow-mapSize={ [ 1024, 1024 ] }
-            shadow-camera-near={ 1 }
-            shadow-camera-far={ 10 }
-            shadow-camera-top={ 5 }
-            shadow-camera-right={ 5 }
-            shadow-camera-bottom={ - 5 }
-            shadow-camera-left={ - 5 }
-        /> */}
-        {/* <ambientLight intensity={ 1.5 } /> */}
-
-        {/* <mesh castShadow position-y={ 1 } position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" envMapIntensity={ envMapIntensity } />
-        </mesh> */}
-
-        {/* <mesh castShadow position-y={ 1 } ref={ cube } position-x={ 2 } scale={ 1.5 }>
-            <boxGeometry />
-            <meshStandardMaterial color="mediumpurple" envMapIntensity={ envMapIntensity } />
-        </mesh> */}
-
-        {/* <mesh receiveShadow position-y={ 0 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" envMapIntensity={ envMapIntensity } />
-        </mesh> */}
-
-        {/* <ContactShadows
-            position={ [ 0, 0, 0 ] }
-            scale={ 10 }
-            resolution={ 512 }
-            far={ 5 }
-            color={ color }
-            opacity={ opacity }
-            blur={ blur }
-        /> */}
-
-        <Stage
-            shadows={ { type: 'contact', opacity: 0.2, blur: 3 } }
-            environment="sunset"
-            preset="portrait"
-            intensity={ 6 }
-        >
-            <mesh position-y={ 1 } position-x={ - 2 }>
-                <sphereGeometry />
-                <meshStandardMaterial color="orange" />
-            </mesh>
-
-            <mesh ref={ cube } position-y={ 1 } position-x={ 2 } scale={ 1.5 }>
-                <boxGeometry />
-                <meshStandardMaterial color="mediumpurple" />
-            </mesh>
-        </Stage>
+        ) }
 
     </>
 }
--- src/index.jsx
+++ src/index.jsx
@@ -7,12 +7,11 @@ const root = ReactDOM.createRoot(document.querySelector('#root'))
 
 root.render(
     <Canvas
-        shadows={ true }
         camera={ {
             fov: 45,
             near: 0.1,
             far: 200,
-            position: [ - 4, 3, 6 ]
+            position: [ 4, - 2, 6 ]
         } }
     >
         <Experience />
