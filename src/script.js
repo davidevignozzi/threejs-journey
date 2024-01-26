6338aa6628e3d85104e0a6ba5a48721cb682951c
Davide Vignozzi <davide.vignozzi@gmail.com>
2024-01-26 13:04:29 +0100

    :truck: Shader patterns

--- src/script.js
+++ src/script.js
@@ -17,46 +17,20 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Textures
- */
-const textureLoader = new THREE.TextureLoader()
-const flagTexture = textureLoader.load('/textures/flag-french.jpg')
-
-/**
  * Test mesh
  */
 // Geometry
 const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
 
-const count = geometry.attributes.position.count
-const randoms = new Float32Array(count)
-
-for(let i = 0; i < count; i++)
-{
-    randoms[i] = Math.random()
-}
-
-geometry.setAttribute('aRandom', new THREE.BufferAttribute(randoms, 1))
-
 // Material
 const material = new THREE.ShaderMaterial({
     vertexShader: testVertexShader,
     fragmentShader: testFragmentShader,
-    uniforms:
-    {
-        uFrequency: { value: new THREE.Vector2(10, 5) },
-        uTime: { value: 0 },
-        uColor: { value: new THREE.Color('orange') },
-        uTexture: { value: flagTexture }
-    }
+    side: THREE.DoubleSide
 })
-
-gui.add(material.uniforms.uFrequency.value, 'x').min(0).max(20).step(0.01).name('frequencyX')
-gui.add(material.uniforms.uFrequency.value, 'y').min(0).max(20).step(0.01).name('frequencyY')
 
 // Mesh
 const mesh = new THREE.Mesh(geometry, material)
-mesh.scale.y = 2 / 3
 scene.add(mesh)
 
 /**
@@ -106,15 +80,8 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 /**
  * Animate
  */
-const clock = new THREE.Clock()
-
 const tick = () =>
 {
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update material
-    material.uniforms.uTime.value = elapsedTime
-
     // Update controls
     controls.update()
 
