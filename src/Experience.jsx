91a9a55b1652fa7e6ba79c07240fc8f39f59fbe7
Davide Vignozzi <davide.vignozzi@gmail.com>
2024-01-26 15:41:09 +0100

    :truck: R3f fun & simple portfolio

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,78 +1,76 @@
-import { OrbitControls } from '@react-three/drei'
-import { Perf } from 'r3f-perf'
-import { DepthOfField, Bloom, Noise, Glitch, ToneMapping, Vignette, EffectComposer } from '@react-three/postprocessing'
-import { GlitchMode, BlendFunction } from 'postprocessing'
-import Drunk from './Drunk.jsx'
-import { useRef } from 'react'
-import { useControls } from 'leva'
+import {
+  Text,
+  Html,
+  ContactShadows,
+  PresentationControls,
+  Float,
+  Environment,
+  useGLTF
+} from '@react-three/drei';
 
-export default function Experience()
-{
-    const drunkRef = useRef()
+export default function Experience() {
+  const computer = useGLTF(
+    'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/macbook/model.gltf'
+  );
 
-    const drunkProps = useControls('Drunk Effect', {
-        frequency: { value: 2, min: 1, max: 20 },
-        amplitude: { value: 0.1, min: 0, max: 1 }
-    })
+  return (
+    <>
+      <color args={['#241a1a']} attach="background" />
 
-    return <>
-        <color args={ [ '#ffffff' ] } attach="background" />
+      <Environment preset="city" />
 
-        <EffectComposer disableNormalPass>
-            {/* <Vignette
-                offset={ 0.3 }
-                darkness={ 0.9 }
-                blendFunction={ BlendFunction.NORMAL }
-            />
-            <Glitch
-                delay={ [ 0.5, 1 ] }
-                duration={ [ 0.1, 0.3 ] }
-                strength={ [ 0.2, 0.4 ] }
-                mode={ GlitchMode.CONSTANT_MILD }
-            />
-            <Noise
-                premultiply
-                blendFunction={ BlendFunction.SOFT_LIGHT }
-            />
-            <Bloom
-                mipmapBlur
-                intensity={ 0.5 }
-                luminanceThreshold={ 0 }
-            />
-            <DepthOfField
-                focusDistance={ 0.025 }
-                focalLength={ 0.025 }
-                bokehScale={ 6 }
-            /> */}
-            <Drunk
-                ref={ drunkRef }
-                { ...drunkProps }
-                blendFunction={ BlendFunction.DARKEN }
-            />
-            <ToneMapping />
-        </EffectComposer>
+      <PresentationControls
+        global
+        rotation={[0.13, 0.1, 0]}
+        polar={[-0.4, 0.2]}
+        azimuth={[-1, 0.75]}
+        config={{ mass: 2, tension: 400 }}
+        snap={{ mass: 4, tension: 400 }}
+      >
+        <Float rotationIntensity={0.4}>
+          <rectAreaLight
+            width={2.5}
+            height={1.65}
+            intensity={65}
+            color={'#ff6900'}
+            rotation={[-0.1, Math.PI, 0]}
+            position={[0, 0.55, -1.15]}
+          />
 
-        <Perf position="top-left" />
+          <primitive
+            object={computer.scene}
+            position-y={-1.2}
+            // rotation-x={ 0.13 }
+          >
+            <Html
+              transform
+              wrapperClass="htmlScreen"
+              distanceFactor={1.17}
+              position={[0, 1.56, -1.4]}
+              rotation-x={-0.256}
+            >
+              <iframe src="https://bruno-simon.com/html/" />
+            </Html>
+          </primitive>
 
-        <OrbitControls makeDefault />
+          <Text
+            font="./bangers-v20-latin-regular.woff"
+            fontSize={1}
+            position={[2, 0.75, 0.75]}
+            rotation-y={-1.25}
+            maxWidth={2}
+          >
+            Davide Vignozzi
+          </Text>
+        </Float>
+      </PresentationControls>
 
-        <directionalLight castShadow position={ [ 1, 2, 3 ] } intensity={ 4.5 } />
-        <ambientLight intensity={ 1.5 } />
-
-        <mesh castShadow position-x={ - 2 }>
-            <sphereGeometry />
-            <meshStandardMaterial color="orange" />
-        </mesh>
-
-        <mesh castShadow position-x={ 2 } scale={ 1.5 }>
-    <boxGeometry />
-    <meshStandardMaterial color="mediumpurple" />
-</mesh>
-
-        <mesh receiveShadow position-y={ - 1 } rotation-x={ - Math.PI * 0.5 } scale={ 10 }>
-            <planeGeometry />
-            <meshStandardMaterial color="greenyellow" />
-        </mesh>
-
+      <ContactShadows
+        position-y={-1.4}
+        opacity={0.4}
+        scale={5}
+        blur={2.4}
+      />
     </>
+  );
 }
