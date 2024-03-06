a613ab11b241aadcee29c1d6515ae056964bad18
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2024-03-06 19:30:25 +0100

    :construction: (Hologram): Fresnel

--- src/shaders/holographic/fragment.glsl
+++ src/shaders/holographic/fragment.glsl
@@ -1,15 +1,27 @@
 uniform float uTime;
 
 varying vec3 vPosition;
+varying vec3 vNormal;
 
 void main() {    
+    // Normal
+    vec3 normal = normalize(vNormal);
 
     //Stripes
     float stripes = mod((vPosition.y - uTime * 0.02) * 20.0, 1.0); // mod -> you send a value as a the first parameter and when that value reaches the second parameter it goes back to 0
     stripes = pow(stripes, 3.0);
 
+    // Fresnel
+    vec3 viewDirection = normalize(vPosition - cameraPosition);
+    float fresnel = dot(viewDirection, normal) + 1.0;
+    fresnel = pow(fresnel, 2.0);
+
+    // Holographic
+    float holographic = stripes * fresnel;
+    holographic += fresnel * 1.25;
+
     // Final color
-    gl_FragColor = vec4(1.0, 1.0, 1.0, stripes);
+    gl_FragColor = vec4(1.0, 1.0, 1.0, holographic);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
--- src/shaders/holographic/vertex.glsl
+++ src/shaders/holographic/vertex.glsl
@@ -1,4 +1,5 @@
 varying vec3 vPosition;
+varying vec3 vNormal;
 
 void main() {
     // Position
@@ -7,6 +8,10 @@ void main() {
     // Final position
     gl_Position = projectionMatrix * viewMatrix * modelPosition;
 
+    // Model normal
+    vec4 modelNormal = modelMatrix * vec4(normal, 0.0);
+
     // Varyings
     vPosition = modelPosition.xyz;
+    vNormal = modelNormal.xyz;
 } 
