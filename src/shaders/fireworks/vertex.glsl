ecbc9405e5d418a72e769644e78c04f32b9956cd
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2024-03-07 15:51:46 +0100

    :sparkles: (Fireworks): Separate chunks

--- src/shaders/fireworks/vertex.glsl
+++ src/shaders/fireworks/vertex.glsl
@@ -5,9 +5,7 @@ uniform float uProgress;
 attribute float aSize;
 attribute float aTimeMultiplier;
 
-float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax) {
-    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
-}
+#include '../includes/remap.glsl'
 
 void main() {
     float progress = uProgress * aTimeMultiplier;
---
+++ src/shaders/includes/remap.glsl
@@ -0,0 +1,3 @@
+float remap(float value, float originMin, float originMax, float destinationMin, float destinationMax) {
+    return destinationMin + (value - originMin) * (destinationMax - destinationMin) / (originMax - originMin);
+}
