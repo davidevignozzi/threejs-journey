b9c71a363344fec53c47d7529a7c8634310dd0eb
Davide Vignozzi <73885963+davidevignozzi@users.noreply.github.com>
2024-03-07 11:41:52 +0100

    :construction: (Fireworks): Textures

--- src/script.js
+++ src/script.js
@@ -82,7 +82,18 @@ renderer.setPixelRatio(sizes.pixelRatio);
 /**
  * Fireworks
  */
-const createFirework = (count, position, size) => {
+const textures = [
+  textureLoader.load('./particles/1.png'),
+  textureLoader.load('./particles/2.png'),
+  textureLoader.load('./particles/3.png'),
+  textureLoader.load('./particles/4.png'),
+  textureLoader.load('./particles/5.png'),
+  textureLoader.load('./particles/6.png'),
+  textureLoader.load('./particles/7.png'),
+  textureLoader.load('./particles/8.png')
+];
+
+const createFirework = (count, position, size, texture) => {
   // Geometry
   const positionArray = new Float32Array(count * 3);
 
@@ -101,6 +112,7 @@ const createFirework = (count, position, size) => {
   );
 
   // Material
+  texture.flipY = false;
   const material = new THREE.ShaderMaterial({
     vertexShader: fireworksVertexShader,
     fragmentShader: fireworksFragmentShader,
@@ -108,8 +120,12 @@ const createFirework = (count, position, size) => {
       uSize: new THREE.Uniform(size),
       uResolution: new THREE.Uniform(
         new THREE.Vector2(sizes.resolution.x, sizes.resolution.y)
-      )
-    }
+      ),
+      uTexture: new THREE.Uniform(texture)
+    },
+    transparent: true,
+    depthWrite: false,
+    blending: THREE.AdditiveBlending
   });
 
   // Points
@@ -121,7 +137,8 @@ const createFirework = (count, position, size) => {
 createFirework(
   100, // Count
   new THREE.Vector3(), // Position
-  0.5 // Size
+  0.5, // Size
+  textures[7] // Texture
 );
 
 /**
--- src/shaders/fireworks/fragment.glsl
+++ src/shaders/fireworks/fragment.glsl
@@ -1,6 +1,11 @@
+uniform sampler2D uTexture;
+
 void main() {
+    // vec4 textureColor = texture(uTexture, gl_PointCoord);
+    float textureAlpha = texture(uTexture, gl_PointCoord).r;
+
     // Final color
-    gl_FragColor = vec4(1.0, 0.0, 1.0, 1.0);
+    gl_FragColor = vec4(1.0, 1.0, 1.0, textureAlpha);
     #include <tonemapping_fragment>
     #include <colorspace_fragment>
 }
